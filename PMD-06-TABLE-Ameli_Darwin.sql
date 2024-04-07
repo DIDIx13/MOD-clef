83527a997356ac603b2526959ceab1276138333e
Darwin Ameli <darwin.ameli@rpn.ch>
2024-04-07 15:43:50 +0200

    add journal_xml table

--- PMD-06-TABLE-Ameli_Darwin.sql
+++ PMD-06-TABLE-Ameli_Darwin.sql
@@ -1861,6 +1861,18 @@ CREATE OR REPLACE TRIGGER fais_AIUD
 ;
 /
 
+-- Table de journalisation xml
+CREATE TABLE journal_xml (
+    numero NUMBER(9) NOT NULL,
+    xml_data CLOB,
+    generation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description VARCHAR2(255),
+    CONSTRAINT PK_journal_xml PRIMARY KEY (numero)
+);
+
+CREATE SEQUENCE seq_journal_xml START WITH 1 INCREMENT BY 1;
+
+
 --
 -- Insert Data
 --
